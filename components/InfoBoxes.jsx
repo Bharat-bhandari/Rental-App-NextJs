d24bf89ecd852a92289412959e60ac8e2ccb6896
Bharat Bhandari <bharatbhandari0302@gmail.com>
2024-02-23 14:16:59 +0530

    loader and error

---
+++ components/InfoBoxes.jsx
@@ -0,0 +1,38 @@
+import InfoBox from "./InfoBox";
+
+const InfoBoxes = () => {
+  return (
+    <section>
+      <div className="m-auto container-xl lg:container">
+        <div className="grid grid-cols-1 gap-4 p-4 rounded-lg md:grid-cols-2">
+          <InfoBox
+            heading="For Renters"
+            backgroundColor="bg-gray-100"
+            buttonInfo={{
+              text: "Browse Properties",
+              link: "/properties",
+              backgroundColor: "bg-black",
+            }}
+          >
+            Find your dream rental property. Bookmark properties and contact
+            owners.
+          </InfoBox>
+          <InfoBox
+            heading="For Property Owners"
+            backgroundColor="bg-blue-100"
+            buttonInfo={{
+              text: "Add Property",
+              link: "/properties/add",
+              backgroundColor: "bg-blue-500",
+            }}
+          >
+            Find your dream rental property. Bookmark properties and contact
+            owners.
+          </InfoBox>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default InfoBoxes;
